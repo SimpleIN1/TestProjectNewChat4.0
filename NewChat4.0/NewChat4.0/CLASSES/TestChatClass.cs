412bfe174b81dc0da5d1f84f073ceba96e13598c
grigoriy <zimforshint_akross@mail.ru>
2022-05-25 07:21:42 +0300

    add new class called testchatclass

---
+++ NewChat4.0/NewChat4.0/CLASSES/TestChatClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewChat4._0.CLASSES
+{
+    class TestChatClass
+    {
+        public string name;
+        public string namech;
+    }
+}
